4ed8255bbb1fe3036eff0289f651906a2974b4c6
hpp <mowencangtian@qq.com>
2024-01-26 14:54:12 +0800

    simplify the import of main.py

--- vllm/entrypoints/deepseek/main.py
+++ vllm/entrypoints/deepseek/main.py
@@ -1,42 +1,19 @@
-import os
-import requests
-import threading
 import argparse
 import asyncio
-import codecs
-import json
-import time
+import os
 from contextlib import asynccontextmanager
-from http import HTTPStatus
-from typing import AsyncGenerator, Dict, List, Optional, Tuple, Union
 
+import fastapi
 from aioprometheus import MetricsMiddleware
 from aioprometheus.asgi.starlette import metrics
-import fastapi
-import uvicorn
-from fastapi import Request
-from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse, StreamingResponse, Response
 
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.engine.metrics import add_global_metrics_labels
-from vllm.entrypoints.openai.protocol import (
-    CompletionRequest, CompletionResponse, CompletionResponseChoice,
-    CompletionResponseStreamChoice, CompletionStreamResponse,
-    ChatCompletionRequest, ChatCompletionResponse,
-    ChatCompletionResponseChoice, ChatCompletionResponseStreamChoice,
-    ChatCompletionStreamResponse, ChatMessage, DeltaMessage, ErrorResponse,
-    LogProbs, ModelCard, ModelList, ModelPermission, UsageInfo)
 from vllm.logger import init_logger
-from vllm.outputs import RequestOutput
-from vllm.sampling_params import SamplingParams
-from vllm.transformers_utils.tokenizer import get_tokenizer
-from vllm.utils import random_uuid
-
-from .vllm_manager import VLLMManager, VLLMManagerArgs
 from .vllm_generator import VLLMGenerator
+from .vllm_manager import VLLMManager, VLLMManagerArgs
 
 TIMEOUT_KEEP_ALIVE = 5  # seconds
 VLLM_HEART_BEAT_INTERVAL = int(os.getenv("VLLM_WORKER_HEART_BEAT_INTERVAL", 15))
